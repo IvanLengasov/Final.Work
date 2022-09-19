06c0247038b7781f1a73a9a5b612efd03fc118f8
IvanLengasov <110249234+IvanLengasov@users.noreply.github.com>
2022-09-19 14:40:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write($"Введите массив: ");
+var number = Console.ReadLine();
+string[] arr = number.Split(',')!.ToArray();
+Console.Write($"[{string.Join(" ", arr)}] -> ");
+GetArray(arr);
+void GetArray(string[] array)
+{
+    Console.Write("[");
+    string[] array2 = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array2.Length; i++)
+        {
+            if (array[i].Length <= 3)
+                {
+                    Console.Write($"{array[i]} ");
+                    Console.Write($",");
+                    count++;                   
+                }
+            }
+       Console.Write("]");
+ }
